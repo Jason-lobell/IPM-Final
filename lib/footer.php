9843dea3a61b6114b3e3407abd4a82a69dd3a01f
Jason Lobell <=>
2022-05-09 15:09:28 -0400

    updated footer

--- lib/footer.php
+++ lib/footer.php
@@ -1,8 +1,7 @@
 <footer>
     <p><a href="../index.php">Site Map</a></p>
-    <p><a href="//aderusso.w3.uvm.edu/cs148/live-final">Andrew DeRusso Final</a></p>
-    <p><a href="//jvlobell.w3.uvm.edu/cs148/live-final">Jay Lobell Final</a></p>
-    <p><a href="//mpursche.w3.uvm.edu/cs148/live-final"></a>Max Pursche Final</p>
+    <p><a href="//aderusso.w3.uvm.edu/cs148/live-Final/index.php">Andrew DeRusso Final</a></p>
+    <p><a href="//jvlobell.w3.uvm.edu/cs148/live-final/index.php">Jay Lobell Final</a></p>
 </footer>
 </body>
 </html>
